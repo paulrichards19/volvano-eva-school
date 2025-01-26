425eb41575fc2dd652bbc0483263d2ab398be2bf
Paul Richards <paul@richards19.com>
2025-01-26 15:18:17 +0000

    runs continuously

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 let dimming = 0
 input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    music.play(music.createSoundExpression(WaveShape.Square, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     for (let brightness = 0; brightness <= 1023; brightness++) {
-        control.waitMicros(3000)
+        control.waitMicros(2000)
         pins.analogWritePin(AnalogPin.P0, brightness)
     }
-    control.waitMicros(100000)
+    control.waitMicros(6000)
     dimming = 1023
     while (dimming >= 0) {
-        control.waitMicros(3000)
+        control.waitMicros(2000)
         pins.analogWritePin(AnalogPin.P0, dimming)
         dimming += -1
     }
-})
-basic.forever(function () {
-	
 })
